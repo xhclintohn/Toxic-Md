87789dfbe50918e022315f36d8d10d7c1d5e3682
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2024-12-01 19:03:07 +0300

    Update zokou.js

--- framework/zokou.js
+++ framework/zokou.js
@@ -6,7 +6,7 @@ function zokou(obj, fonctions) {
         infoComs.categorie = "General";
     }
     if (!obj.reaction) {
-        infoComs.reaction = "🥰";
+        infoComs.reaction = "☣️";
     }
     infoComs.fonction = fonctions;
     cm.push(infoComs);
