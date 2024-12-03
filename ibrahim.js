762a63f006d24cfc432d844d1460f42e14b0472b
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-12-03 09:54:02 +0300

    Fixed🗡️⚔️

--- ibrahim.js
+++ ibrahim.js
@@ -249,6 +249,19 @@ function mybotpic() {
                 mybotpic
             
             };
+             // AUTO_READ_MESSAGES: Automatically mark messages as read if enabled.
+      if (conf.AUTO_READ_MESSAGES === "yes") {
+        zk.ev.on("messages.upsert", async m => {
+          const {
+            messages
+          } = m;
+          for (const message of messages) {
+            if (!message.key.fromMe) {
+              await zk.readMessages([message.key]);
+            }
+          }
+        });
+      }
 
 
             /************************ anti-delete-message */
