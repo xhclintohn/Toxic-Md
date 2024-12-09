7657c28537b837d73047c0b8b815264c7db38837
𝐱𝐡_𝐜𝐥𝐢𝐧𝐭𝐨𝐧 <152104512+xhclintohn@users.noreply.github.com>
2024-12-09 13:51:38 +0300

    Add files via upload

---
+++ framework/imgur.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const fs = require('fs');
+const FormData = require('form-data');
+
+async function uploadImageToImgur(imagePath, clientId) {
+  try {
+    const data = new FormData();
+    data.append('image', fs.createReadStream(imagePath));
+
+    const headers = {
+      'Authorization': `Client-ID ${clientId}`,
+      ...data.getHeaders()
+    };
+
+    const config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'https://api.imgur.com/3/image',
+      headers: headers,
+      data: data
+    };
+
+    const response = await axios(config);
+    const imageUrl = response.data.data.link;
+    return imageUrl;
+  } catch (error) {
+    console.error('Error while\'uploading to Imgur:', error);
+    throw new Error('An error occurred while\'upload to Imgur.');
+  }
+}
+
+module.exports = { uploadImageToImgur };
